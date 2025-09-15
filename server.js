7051aea5235876b4c16fc230c58692ada7571887
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-09-15 22:35:01 +0530

    started with the project and did basic set up

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require('./src/app')
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+ 
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  // ...
+}); 
+
+
+
+httpServer.listen(3000,()=>{
+    console.log("Server running on port 3000")
+})
