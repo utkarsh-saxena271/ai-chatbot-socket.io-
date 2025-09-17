46d718d59afee82e3c89f475252ea41b9fff5f29
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-09-17 23:03:54 +0530

    integrated ai, and created socket events for response and promp input outputs

--- server.js
+++ server.js
@@ -2,16 +2,28 @@ require('dotenv').config()
 const app = require('./src/app')
 const { createServer } = require("http");
 const { Server } = require("socket.io");
- 
+const generateResponse = require('./src/services/ai.service')
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-  // ...
+  console.log('a user connected')
+  // socket.on("disconnect",()=>{
+  //   console.log("a user disconnected")
+  // })
+
+  // socket.on('event',(data)=>{console.log(data)}) //custom event
+  socket.on('aiMessage',async (data)=>{
+    console.log("recieved prompt", data.prompt)
+    const response = await generateResponse(data.prompt);
+    console.log(response)
+
+    socket.emit('ai-message-response',{response})
+  })
 }); 
 
-
+ 
 
 httpServer.listen(3000,()=>{
     console.log("Server running on port 3000")
